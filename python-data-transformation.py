edf24f8e697c9a60cdf4978d5e6bdf249490b380
Mandeep Makkar <mandeepmakkar@gmail.com>
2020-06-06 13:19:01 -0400

    added comments

--- python-data-transformation.py
+++ python-data-transformation.py
@@ -1,5 +1,5 @@
 import csv
-
+#read csv1
 f = open('attendees1.csv')
 csv_f = csv.reader(f)
 
@@ -8,7 +8,7 @@ attendee_emails1 = []
 for row in csv_f:
   attendee_emails1.append(row[2])
 
-
+#read csv 2
 f = open('attendees2.csv')
 csv_f = csv.reader(f)
 
@@ -17,9 +17,12 @@ attendee_emails2 = []
 for row in csv_f:
   attendee_emails2.append(row[2])
 
+#create set out of list - so that we have unique entries
 attendee_emails1 = set(attendee_emails1)
 attendee_emails2 = set(attendee_emails2)
 
+#find the difference between 2 sets
 second_year_attendees = attendee_emails2.difference(attendee_emails1)
 
-print second_year_attendees
+#print the difference
+print(second_year_attendees)
